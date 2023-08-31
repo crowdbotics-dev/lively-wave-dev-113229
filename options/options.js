eaf76a6066d67f6d3ce9af69d5e7a11a8df5a583
Crowdbotics <engineering@crowdbotics.com>
2023-08-31 12:39:34 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "lively-wave-dev-113229",
+  url: "https://lively-wave-dev-113229.botics.co",
+  api: "https://lively-wave-dev-113229.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
